0fda6be1a0d755059a3ea3cfc9e968503572d807
SujayVullupala123 <75144182+SujayVullupala123@users.noreply.github.com>
2023-06-29 18:25:47 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("JFrame");
+		frame.setSize(420, 420);
+		frame.setVisible(true);
+		
+		
+
+	}
+
+}
